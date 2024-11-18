f0a3d3db314b03e355ff27c331b8ad61c4b4380c
ahodzi <ahodzic@simplifeye.co>
2024-11-18 19:31:00 +0100

    new tests for contat form

--- cypress/e2e/1-getting-started/first-spec.cy.js
+++ cypress/e2e/1-getting-started/first-spec.cy.js
@@ -16,4 +16,13 @@ describe('Example tests', () => {
       .should('be.visible')
       .and('not.be.disabled')
   })
+
+  it.only('Navigate to contact us form', () => {
+    cy.get('a[href*="contact"]').should('be.visible').click()
+    cy.url().should('contain', 'contact_us')
+    cy.contains('h2', 'contact us', { matchCase: false }).should('be.visible')
+    cy.get('div.bg').find('h2').contains('contact us', { matchCase: false })
+    cy.get('h2').first().should('be.visible') // Contact us element
+    cy.get('h2').eq(1).should('be.visible') // get in touch element
+  })
 })
